bbf9d4d754cf9a22625ffaf8ba311beea23d9ae7
RobXela <--global>
2022-06-14 18:47:05 +0300

    Changed getLocalStorage structure

--- src/app/core/services/localStorage.service.ts
+++ src/app/core/services/localStorage.service.ts
@@ -3,41 +3,49 @@ import { Note } from "src/app/features/records/records/records.component";
 
 @Injectable({ providedIn: 'root' })
 export class LocalStorageService {
-    public notes!: Note;
+    public notes: Note =
+      {
+        title: "Мой блокнот",
+        date: "",
+        time: "",
+        children: [
+          {
+            type: "root",
+            title: "Заметки",
+            date: "",
+            time: "",
+            children: []
+          },
+          {
+            type: "folder",
+            title: "Тачки",
+            date: "",
+            time: "",
+            children: []
+          },
+          {
+            type: "folder",
+            title: "Свиданки",
+            date: "",
+            time: "",
+            children: []
+          },
+          {
+            type: "inbox",
+            title: "Входящие",
+            date: "",
+            time: "",
+            children: []
+          },
+        ]
+      }
 
     getLocalStorage(item: string) {
         const data = localStorage.getItem(item);
         if (data != null) {
           return JSON.parse(data)
         } else if (item === 'notes') {
-            return {
-              title: "Мой блокнот",
-              date: "",
-              time: "",
-              children: [
-                {
-                  type: "inbox",
-                  title: "Входящие",
-                  date: "",
-                  time: "",
-                  children: []
-                },
-                {
-                  type: "knowledge",
-                  title: "Знание",
-                  date: "",
-                  time: "",
-                  children: []
-                },
-                {
-                  type: "task",
-                  title: "Задача",
-                  date: "",
-                  time: "",
-                  children: []
-                },
-              ]
-            } 
+            return this.notes
         } else {
             return {}
         }
