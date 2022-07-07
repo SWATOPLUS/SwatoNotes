c948502d18764093471244c54b4ab9093317fe66
RobXela <--global>
2022-07-07 19:07:55 +0300

    Modified move item in array in Projects component

--- src/app/features/notes/projects/projects.component.ts
+++ src/app/features/notes/projects/projects.component.ts
@@ -6,12 +6,16 @@ import { Title } from '@angular/platform-browser';
 import { NGXLogger } from 'ngx-logger';
 import { AuthenticationService } from 'src/app/core/services/auth.service';
 import { Note, NoteService } from 'src/app/core/services/notes.service';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
   currentUser: any;
@@ -19,20 +23,19 @@ export class ProjectsComponent implements OnInit {
   public project: Note | any;
   public inbox: Note | any;
   public subProjects: any;
-  public notes: Note [] = [];
+  public notes: Note[] = [];
   public inboxRecord: Note[] = [];
   public recordsListId: string = 'records';
   public projectsListId: string = 'projects';
   public notesListId: string = 'notes';
   private subscription: Subscription;
 
-  public notesPlaceholder: string = "Новая заметка";
-  public notesHeader: string = "Заметки";
-  public projectPlaceholder: string = "Новый проект";
-  public projectHeader: string = "Проекты";
-  public recordsPlaceholder: string = "Новая запись";
-  public recordsHeader: string = "Входящие записи";
-
+  public notesPlaceholder: string = 'Новая заметка';
+  public notesHeader: string = 'Заметки';
+  public projectPlaceholder: string = 'Новый проект';
+  public projectHeader: string = 'Проекты';
+  public recordsPlaceholder: string = 'Новая запись';
+  public recordsHeader: string = 'Входящие записи';
 
   displayDate(note: Note) {
     return note.creationLocalDateTime;
@@ -42,25 +45,34 @@ export class ProjectsComponent implements OnInit {
     this.noteService.removeNote(note.id);
     this.reload();
   }
-  
+
   addNote(newNoteText: string) {
     this.noteService.addNote(newNoteText, 'note', this.project.id);
     this.reload();
   }
-  
+
   addProject(newProjectText: string) {
-    this.noteService.addNote(newProjectText, "project", this.project.id);
+    this.noteService.addNote(newProjectText, 'project', this.project.id);
     this.reload();
   }
-  
+
   addRecord(newRecordText: string) {
-    this.noteService.addNote(newRecordText, "note", this.inbox.id);
+    this.noteService.addNote(newRecordText, 'note', this.inbox.id);
     this.reload();
   }
 
   drop(event: CdkDragDrop<Note[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      let eventNote = event.container.data[event.currentIndex];
+      let note = this.noteService.getNote(eventNote.id);
+      const eventNeighbor = event.container.data[event.currentIndex + 1];
+      const neighborNote = this.noteService.getNote(eventNeighbor.id); 
+      this.noteService.moveNote(note.id, neighborNote.id);
     } else {
       transferArrayItem(
         event.previousContainer.data,
@@ -68,11 +80,9 @@ export class ProjectsComponent implements OnInit {
         event.previousIndex,
         event.currentIndex
       );
-    }
-    let eventNote = event.container.data[event.currentIndex];
-    let note = this.noteService.getNote(eventNote.id);
-    if(event.container.id != event.previousContainer.id) {
-      switch(event.container.id) {
+      let eventNote = event.container.data[event.currentIndex];
+      let note = this.noteService.getNote(eventNote.id);
+      switch (event.container.id) {
         case 'notes':
           this.noteService.setKey(note.id, undefined, undefined, 'note');
           this.noteService.setParent(note.id, this.project.id);
@@ -90,26 +100,38 @@ export class ProjectsComponent implements OnInit {
   }
 
   reload() {
-    this.notes = this.noteService.getChildren(this.project.id).filter(x => x.type === 'note');
-    this.subProjects = this.noteService.getChildren(this.project.id).filter(x => x.type === 'project');
+    this.notes = this.noteService
+      .getChildren(this.project.id)
+      .filter((x) => x.type === 'note');
+    this.subProjects = this.noteService
+      .getChildren(this.project.id)
+      .filter((x) => x.type === 'project');
     this.inboxRecord = this.noteService.getChildren(this.inbox.id);
   }
-  
+
   reloadProject(id: string) {
     this.project = this.noteService.getNote(id);
     this.inbox = this.noteService.getTool(this.project.id, 'inbox');
     this.inboxRecord = this.noteService.getChildren(this.inbox.id);
-    this.subProjects = this.noteService.getChildren(this.project?.id).filter(x => x.type === 'project');
-    this.notes = this.noteService.getChildren(this.project?.id).filter(x => x.type === 'note');
+    this.subProjects = this.noteService
+      .getChildren(this.project?.id)
+      .filter((x) => x.type === 'project');
+    this.notes = this.noteService
+      .getChildren(this.project?.id)
+      .filter((x) => x.type === 'note');
   }
 
-  constructor(private notificationService: NotificationService,
+  constructor(
+    private notificationService: NotificationService,
     private authService: AuthenticationService,
     private titleService: Title,
     private logger: NGXLogger,
     private noteService: NoteService,
-    private activatedRoute: ActivatedRoute) {
-      this.subscription = this.activatedRoute.params.subscribe(params => this.reloadProject(params["id"]));
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.subscription = this.activatedRoute.params.subscribe((params) =>
+      this.reloadProject(params['id'])
+    );
   }
 
   ngOnInit() {
